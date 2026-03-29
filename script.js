03098faed273aef8a3f6efa3e94da944db8852fa
mitchmaaan94 <mitchmaaan94@gmail.com>
2026-03-29 13:41:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+window.onload = () => {
+  setTimeout(() => {
+    document.getElementById("loader").style.opacity = "0";
+    setTimeout(() => {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("content").classList.remove("hidden");
+    }, 600);
+  }, 1800);
+};
