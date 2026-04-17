18412d947c0c4d0fa67dffce924756f6520821fb
mitchmaaan94 <mitchmaaan94@gmail.com>
2026-04-17 10:16:16 +0530

    Update script.js
    Update for keeping Header locked

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ window.onload = () => {
       document.getElementById("content").classList.remove("hidden");
       // === Scroll Reveal Logic ===
       const heroes = document.querySelectorAll(".hero-image");
-
+      const header = document.querySelector(".header");
+      
       const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
           const watermark = entry.target.querySelector(".watermark");
@@ -24,6 +25,16 @@ window.onload = () => {
 
       heroes.forEach(hero => observer.observe(hero));
       // === End Scroll Reveal Logic ===
+       // === Header fade after 25% scroll ===
+      window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY;
+        const viewportHeight = window.innerHeight;
+        if (scrollY > viewportHeight * 0.25) {
+          header.style.opacity = "0";
+        } else {
+          header.style.opacity = "1";
+        }
+      });
     }, 600);
   }, 2000);
 };
